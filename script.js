95bb099d69b4d205c4f188cb713bb5b3588ef43f
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-28 12:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let fname = document.getElementById('f1');
+let lname = document.getElementById('l1');
+let phoneNum = document.getElementById('p1');
+let email = document.getElementById('e1');
+let submit = document.getElementById('btn');
+
+submit.onclick = function () {
+	alert(`First Name: ${fname.value} Last Name: ${lname.value} Phone Number: ${phoneNum.value} Email ID: ${email.value}`);
+};
